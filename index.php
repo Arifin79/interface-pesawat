dacd26d085a4a4dda3dd602cfd7c1423aea507c3
windini <aquariuswin1424@gmail.com>
2023-11-06 12:36:28 +0700

    update codingan windi

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include_once "Jet.php";
+include_once "Kargo.php";
 
 $pesawat = new Jet();
 $pesawat->takeOff();
@@ -7,4 +8,9 @@ echo "<br>";
 $pesawat->manuver();
 echo "<br>";
 
+$pesawat = new Kargo();
+$pesawat->takeOff();
+echo "<br>";
+$pesawat->manuver();
+echo "<br>";
 ?>
